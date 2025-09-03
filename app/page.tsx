f9c768d2e83addea756c9931feb9ac81746ecdb0
Patric Schiess <patric@patrics-macbook-pro.home>
2025-09-03 12:05:53 +0200

    default build final

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,19 @@
+import CallToAction from "@/components/call-to-action";
+import ContentSection from "@/components/content-7";
+import FeaturesSection from "@/components/features-8";
 import HeroSection from "@/components/hero-section";
+import Testimonials from "@/components/testimonials";
+import ContentSectionBottom from "@/components/content-1";  
+import FooterSection from "@/components/footer";
 
 export default function Home() {
   return <div>
     <HeroSection />
+    <FeaturesSection />
+    <ContentSection />
+    <CallToAction />
+    <Testimonials />
+    <ContentSectionBottom />
+    <FooterSection />
   </div>;
 }
